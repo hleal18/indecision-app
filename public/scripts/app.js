db90f670819abb69c206ce59633fdcd8c92348ab
Humberto Leal <humberto.adolfo@hotmail.com>
2017-11-26 02:13:06 -0500

    Clase 10: instrucciones JSX.

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,13 +1,65 @@
 'use strict';
 
+//Se buscan hacer templates dinamicas.
 console.log('App.js is running');
 
+//La siguiente linea da error. Solo pueden haber etiquetas encerradas en una raíz.
+//De forma que solo haya una etiqueta de cierre de una sola jerarquia.
+//var template = <h1> Indecision App. </h1><p>This is some info</p>;
+//Se soluciona envolviendo lo anterior en un div. De la siguiente forma:
+//Se le conoce como wrapper div.
+//Se le agregan paréntesis como buena práctica, para poder identificar las etiquetas
+//Que hacen parte del la variable template.
 var template = React.createElement(
-  'h1',
-  null,
-  ' Indecision App. '
+    'div',
+    null,
+    React.createElement(
+        'h1',
+        null,
+        'Humberto Leal'
+    ),
+    React.createElement(
+        'p',
+        null,
+        'This is some info'
+    ),
+    React.createElement(
+        'ol',
+        null,
+        React.createElement(
+            'li',
+            null,
+            'Item in'
+        ),
+        React.createElement(
+            'li',
+            null,
+            'Item out'
+        )
+    )
+);
+
+//Challenge
+var templateTwo = React.createElement(
+    'div',
+    null,
+    React.createElement(
+        'h1',
+        null,
+        'Humberto Leal'
+    ),
+    React.createElement(
+        'p',
+        null,
+        'Edad: 18'
+    ),
+    React.createElement(
+        'p',
+        null,
+        'Ciudad: Cartagena'
+    )
 );
 
 var appRoot = document.getElementById('app');
 
-ReactDOM.render(template, appRoot);
+ReactDOM.render(templateTwo, appRoot);
--- src/app.js
+++ src/app.js
@@ -1,7 +1,33 @@
+//Se buscan hacer templates dinamicas.
 console.log('App.js is running');
 
-var template = <h1> Indecision App. </h1>;
+//La siguiente linea da error. Solo pueden haber etiquetas encerradas en una raíz.
+//De forma que solo haya una etiqueta de cierre de una sola jerarquia.
+//var template = <h1> Indecision App. </h1><p>This is some info</p>;
+//Se soluciona envolviendo lo anterior en un div. De la siguiente forma:
+//Se le conoce como wrapper div.
+//Se le agregan paréntesis como buena práctica, para poder identificar las etiquetas
+//Que hacen parte del la variable template.
+var template = (
+    <div>
+        <h1>Humberto Leal</h1>
+        <p>This is some info</p>
+        <ol>
+            <li>Item in</li>
+            <li>Item out</li>
+        </ol>
+    </div>
+);
+
+//Challenge
+var templateTwo = (
+    <div>
+        <h1>Humberto Leal</h1>
+        <p>Edad: 18</p>
+        <p>Ciudad: Cartagena</p>
+    </div>
+);
 
 var appRoot = document.getElementById('app');
 
-ReactDOM.render(template, appRoot);
+ReactDOM.render(templateTwo, appRoot);
