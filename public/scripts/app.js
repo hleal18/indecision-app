2ba7a186e3de8eba20ddc8ac35df1dd6a01838c1
Humberto Leal <humberto.adolfo@hotmail.com>
2017-11-26 11:34:34 -0500

    Clase 12: Tres tecnicas de condicionales, atributo options de Indecision.

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -4,9 +4,16 @@
 //if y ternarios.
 console.log('App.js is running');
 
+/**
+ * Reto
+ * Renderizar solo el subtitulo y la etiqueta p si el subtitulo existe
+ * Usar el operador and.
+ * Renderizar un parrafo si hay elementos en el arreglo.
+ */
 var app = {
     title: 'Indecision',
-    subtitle: 'Put your life in the hands of a computer'
+    subtitle: 'Put your life in the hands of a computer',
+    options: ['One', 'Two']
 };
 
 var template = React.createElement(
@@ -17,10 +24,15 @@ var template = React.createElement(
         null,
         app.title
     ),
-    React.createElement(
+    app.subtitle && React.createElement(
         'p',
         null,
         app.subtitle
+    ),
+    React.createElement(
+        'p',
+        null,
+        app.options.length > 0 ? 'Here are your options.' : 'No options.'
     ),
     React.createElement(
         'ol',
@@ -42,40 +54,46 @@ var template = React.createElement(
 var user = {
     name: 'Humberto',
     age: 18,
-    location: 'Cartagena'
+    location: 'Suiza'
 };
 
 //Se combinan las etiquetas con expresiones javascript. No se permiten declaraciones, como las if.
-//Para usar if, se plantean las funcions.
+//Para usar if, se plantean las funciones.
+//Como al mostrar el Unknown aun se usa la etiqueta parrafo, se busca que se reemplace de acuerdo
+//Al valor de retorno.
 function getLocation(location) {
     if (location) {
-        return location;
-    } else {
-        return 'Unknown';
-    }
+        return React.createElement(
+            'p',
+            null,
+            'Location: ',
+            location
+        );
+    } //No se le pone nada despues, se asume un 'undefined'
 }
+//Si la expresion JSX es undefined, nada se muestra.
+//Se usan operadores ternarios con username.
+//Los valores booleanos y null son ignorados por jsx.
 var templateTwo = React.createElement(
     'div',
     null,
     React.createElement(
         'h1',
         null,
-        user.name
+        user.name ? user.name : 'Anonymous'
     ),
-    React.createElement(
+
+    //Se observa la técnica, cuando es true, se evalúa el segundo condicional,
+    //Por tanto, se muestra la etiqueta de párrafo.            
+    user.age && user.age >= 18 && React.createElement(
         'p',
         null,
-        'Edad: ',
+        'Age: ',
         user.age
     ),
-    React.createElement(
-        'p',
-        null,
-        'Ciudad: ',
-        getLocation(user.location)
-    )
+    getLocation(user.location)
 );
 
 var appRoot = document.getElementById('app');
 
-ReactDOM.render(templateTwo, appRoot);
+ReactDOM.render(template, appRoot);
--- src/app.js
+++ src/app.js
@@ -2,15 +2,23 @@
 //if y ternarios.
 console.log('App.js is running');
 
+/**
+ * Reto
+ * Renderizar solo el subtitulo y la etiqueta p si el subtitulo existe
+ * Usar el operador and.
+ * Renderizar un parrafo si hay elementos en el arreglo.
+ */
 var app = {
     title: 'Indecision',
-    subtitle: 'Put your life in the hands of a computer'
+    subtitle: 'Put your life in the hands of a computer',
+    options: ['One', 'Two']
 };
 
 var template = (
     <div>
         <h1>{app.title}</h1>
-        <p>{app.subtitle}</p>
+        {(app.subtitle) && <p>{app.subtitle}</p>}
+        <p>{app.options.length > 0 ? 'Here are your options.' : 'No options.'}</p>
         <ol>
             <li>Item in</li>
             <li>Item out</li>
@@ -22,26 +30,33 @@ var template = (
 var user = {
     name: 'Humberto',
     age: 18,
-    location: 'Cartagena'
+    location: 'Suiza'
 };
 
 //Se combinan las etiquetas con expresiones javascript. No se permiten declaraciones, como las if.
-//Para usar if, se plantean las funcions.
+//Para usar if, se plantean las funciones.
+//Como al mostrar el Unknown aun se usa la etiqueta parrafo, se busca que se reemplace de acuerdo
+//Al valor de retorno.
 function getLocation(location){
     if (location) {
-        return location;
-    } else {
-        return 'Unknown';
-    }
+        return <p>Location: {location}</p>;
+    }//No se le pone nada despues, se asume un 'undefined'
 }
+//Si la expresion JSX es undefined, nada se muestra.
+//Se usan operadores ternarios con username.
+//Los valores booleanos y null son ignorados por jsx.
 var templateTwo = (
     <div>
-        <h1>{user.name}</h1>
-        <p>Edad: {user.age}</p>
-        <p>Ciudad: {getLocation(user.location)}</p>
+        <h1>{user.name ? user.name : 'Anonymous'}</h1>
+        {
+            //Se observa la técnica, cuando es true, se evalúa el segundo condicional,
+            //Por tanto, se muestra la etiqueta de párrafo.            
+            (user.age && user.age >= 18) && <p>Age: {user.age}</p>
+        }        
+        {getLocation(user.location)}
     </div>
 );
 
 var appRoot = document.getElementById('app');
 
-ReactDOM.render(templateTwo, appRoot);
+ReactDOM.render(template, appRoot);
