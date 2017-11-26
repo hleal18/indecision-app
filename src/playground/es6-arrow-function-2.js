fc5dee2a10da871bae9743b4c322bf28376a9aed
Humberto Leal <humberto.adolfo@hotmail.com>
2017-11-26 12:52:54 -0500

    Clase 15: Inicial, posibilidades y detalles de las funciones dentro de un objeto. En la siguiente se eliminan comentarios y se usa la version definitiva del tutorial.

--- src/playground/es6-arrow-function-2.js
+++ src/playground/es6-arrow-function-2.js
@@ -1,9 +1,7 @@
-'use strict';
-
 //El objeto arguments no esta disponible en las arrow functions.
-var add = function add(a, b) {
+const add = function (a, b) {
     console.log(arguments);
-    return a + b;
+    return a + b;  
 };
 console.log(add(55, 1));
 
@@ -15,19 +13,17 @@ console.log(add(55, 1));
 // console.log(add(a+b));
 
 //la keyword this, no estan delimitadas en las arrow functions.
-var user = {
+const user = {
     name: 'Humberto',
     cities: ['Cartagena', 'Medellin'],
-    printPlacesLived: function printPlacesLived() {
-        var _this = this;
-
-        var that = this;
+    printPlacesLived: function () {
+        const that = this;
 
         //No se puede, this no está disponible.
         // this.cities.forEach( function (city) {
         //     console.log(this.name + ' has lived in: ' + city);
         // })
-
+        
         //Solucion con funciones ES5
         // this.cities.forEach( function (city) {
         //     console.log(that.name + ' has lives in: ' + city);
@@ -35,9 +31,9 @@ var user = {
 
         //Con las Arrow no es necesario that = this
         //Automáticamente conoce el this del ambiente padre.
-        this.cities.forEach(function (city) {
-            console.log(_this.name + ' has lived in: ' + city);
-        });
+        this.cities.forEach( (city) => {
+            console.log(this.name + ' has lived in: ' +  city);
+        })
     }
     //Con arrow desde aquí, no reconoce el this, se va al objeto
     // //Del ambiente padre, el cual es global.
