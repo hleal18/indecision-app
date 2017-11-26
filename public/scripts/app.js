8ac68b058101720a7048b23d5f5278fdff70c5d6
Humberto Leal <humberto.adolfo@hotmail.com>
2017-11-26 13:08:01 -0500

    Clase 15: Finalización de características ES6 y exploración del map de arreglos.

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,51 +1,39 @@
 'use strict';
 
-//El objeto arguments no esta disponible en las arrow functions.
 var add = function add(a, b) {
-    console.log(arguments);
+    //console.log(arguments);
     return a + b;
 };
 console.log(add(55, 1));
 
-//No se puede.
-// add = (a, b) => {
-//     console.log(arguments);
-//     return a + b;
-// }
-// console.log(add(a+b));
-
-//la keyword this, no estan delimitadas en las arrow functions.
 var user = {
     name: 'Humberto',
     cities: ['Cartagena', 'Medellin'],
     printPlacesLived: function printPlacesLived() {
         var _this = this;
 
-        var that = this;
-
-        //No se puede, this no está disponible.
-        // this.cities.forEach( function (city) {
-        //     console.log(this.name + ' has lived in: ' + city);
-        // })
-
-        //Solucion con funciones ES5
-        // this.cities.forEach( function (city) {
-        //     console.log(that.name + ' has lives in: ' + city);
-        // })
-
-        //Con las Arrow no es necesario that = this
-        //Automáticamente conoce el this del ambiente padre.
-        this.cities.forEach(function (city) {
-            console.log(_this.name + ' has lived in: ' + city);
+        //Nueva forma de recorrer arreglos.
+        //Permite transformar cada item.
+        return this.cities.map(function (city) {
+            return _this.name + ' has lived in: ' + city;
         });
     }
-    //Con arrow desde aquí, no reconoce el this, se va al objeto
-    // //Del ambiente padre, el cual es global.
-    // printPlacesLivedArrow: () => {
-    //     this.cities.forEach( (city) => {
-    //         console.log(this.name + ' has lived in: ' + city);
-    //     })
-    // };
 };
 
-user.printPlacesLived();
+console.log(user.printPlacesLived());;
+
+//Challenge area
+
+var multiplier = {
+    numbers: [10, 20, 30],
+    multiplyBy: 2,
+    multiply: function multiply() {
+        var _this2 = this;
+
+        return this.numbers.map(function (number) {
+            return number * _this2.multiplyBy;
+        });
+    }
+};
+
+console.log(multiplier.multiply());
--- src/playground/es6-arrow-function-2.js
+++ src/playground/es6-arrow-function-2.js
@@ -1,47 +1,29 @@
-//El objeto arguments no esta disponible en las arrow functions.
-const add = function (a, b) {
-    console.log(arguments);
+const add = (a, b) => {
+    //console.log(arguments);
     return a + b;  
 };
 console.log(add(55, 1));
 
-//No se puede.
-// add = (a, b) => {
-//     console.log(arguments);
-//     return a + b;
-// }
-// console.log(add(a+b));
-
-//la keyword this, no estan delimitadas en las arrow functions.
 const user = {
     name: 'Humberto',
     cities: ['Cartagena', 'Medellin'],
-    printPlacesLived: function () {
-        const that = this;
-
-        //No se puede, this no está disponible.
-        // this.cities.forEach( function (city) {
-        //     console.log(this.name + ' has lived in: ' + city);
-        // })
-        
-        //Solucion con funciones ES5
-        // this.cities.forEach( function (city) {
-        //     console.log(that.name + ' has lives in: ' + city);
-        // })
-
-        //Con las Arrow no es necesario that = this
-        //Automáticamente conoce el this del ambiente padre.
-        this.cities.forEach( (city) => {
-            console.log(this.name + ' has lived in: ' +  city);
-        })
-    }
-    //Con arrow desde aquí, no reconoce el this, se va al objeto
-    // //Del ambiente padre, el cual es global.
-    // printPlacesLivedArrow: () => {
-    //     this.cities.forEach( (city) => {
-    //         console.log(this.name + ' has lived in: ' + city);
-    //     })
-    // };
+    printPlacesLived() {
+        //Nueva forma de recorrer arreglos.
+        //Permite transformar cada item.
+        return this.cities.map((city) => this.name + ' has lived in: ' + city);        
+    }    
 };
 
-user.printPlacesLived();
+console.log(user.printPlacesLived());;
+
+//Challenge area
+
+const multiplier = {
+    numbers: [10, 20, 30],
+    multiplyBy: 2,
+    multiply(){
+        return this.numbers.map( (number) => number * this.multiplyBy);
+    }
+};
+
+console.log(multiplier.multiply());
