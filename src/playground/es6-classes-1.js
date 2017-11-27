7391fe935b30715bf7e2c0496c8dce3e56bf7788
Humberto Leal <humberto.adolfo@hotmail.com>
2017-11-26 23:47:04 -0500

    Clase 25: Extender una clase y sobreescritura en clases hijas.

--- src/playground/es6-classes-1.js
+++ src/playground/es6-classes-1.js
@@ -15,8 +15,44 @@ class Person {
     }
 }
 
-const me = new Person('Humberto Leal', 90);
-console.log(me.getDescription());
+//se va a crear una subclase de Person
+class Student extends Person {
+    constructor(name, age, major){
+        super(name, age);
+        this.major = major;        
+    }
+    hasMajor(){
+        return this.major;
+    }
+    getDescription() {
+        let description = super.getDescription();
+        
+        if(this.hasMajor()) {
+            description += `. Their major is ${this.major}`
+        }
 
-const other = new Person();
-console.log(me.getDescription());
+        return description;
+    }
+}
+
+class Traveler extends Person {
+    constructor(name, age, homeLocation) {
+        super(name, age);
+        this.homeLocation = homeLocation;
+    }
+    getGretting(){
+        let gretting = super.getGretting();
+        if(this.homeLocation){
+            gretting += ` I'm visiting from ${this.homeLocation}`;
+        }
+        return gretting;
+    }
+}
+
+
+
+const me = new Traveler('Humberto Leal', 90, 'Colombia');
+console.log(me.getGretting());
+
+const other = new Student();
+console.log(other.getGretting());
