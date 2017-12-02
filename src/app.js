69e3e460708f5cee42040d652e5ff089f0ea7579
Humberto Leal <humberto.adolfo@hotmail.com>
2017-12-01 23:10:55 -0500

    Clase 53: se instala react y reactdom localmente y se muestra como se deben importar en archivo js con la sintaxis es6. Se demuestra que no se cuenta con un compilador que anteriormente se contaba, el cual era babel.

--- src/app.js
+++ src/app.js
@@ -1,24 +1,13 @@
-//Al importarlo se ejecuta la llamada a la consola.
-//import './utils.js'
-//Para importar funcionalidades especificas
-//El archivo a requerir debe especificar los valores
-//exportables.
-//Se obtiene específicamente la funcionalidad requerida.
-//Los nombres de las funcionalidades a obtener tienen que coincidir.
-//substract se importa como default, debido a que lo es.
-//el default puede referenciarse con cualquier nombre.
-// import restica, { square, add } from './utils.js';
+//install - import - use
+import React from 'react';
+import ReactDOM from 'react-dom';
 
-// // console.log("app.js is running!");
-
-// // //Cada archivo mantiene su propio ambiente y alcance.
-// // //No se tiene acceso a lo siguiente:
-// console.log(square(4));
-// console.log(add(100,23));
-// console.log(restica(100, 81));
-
-import isSenior, {isAdult, canDrink } from './person.js';
-
-console.log(isAdult(25));
-console.log(canDrink(21));
-console.log(isSenior(35));
+//Si no se está usando babel, esto da error.
+//const template = <p>testing 123</p>;
+//Esto se renderiza correctamente pero no es lo que se busca.
+// const template = React.createElement(
+//     'p',
+//     {},
+//     'testing 123'
+// );
+ReactDOM.render(template, document.getElementById('app'));
