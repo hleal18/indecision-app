db28bf0e125d86f9ea2e453a8f779841a441e6af
Humberto Leal <humberto.adolfo@hotmail.com>
2017-12-02 22:26:29 -0500

    Clase 72: Se termina de estilizar la app tanto para escritorio como móviles. Se optimizan espaciados entre componentes visuales mediante media queries. Se finaliza el desarrollo de la app.

--- src/components/OptionModal.js
+++ src/components/OptionModal.js
@@ -6,11 +6,13 @@ const OptionModal = (props) => (
         isOpen={!!props.selectedOption}
         //Al pasarle una función,permite que se cierra mediante esc o clicar el fondo de la página.
         onRequestClose={props.handleClearSelectedOption}
+        closeTimeoutMS={200}
         contentLabel="Selected Option"
+        className="modal"
     >
-        <h3>Selected Option</h3>
-        {props.selectedOption && <p>{props.selectedOption}</p>}
-        <button onClick={props.handleClearSelectedOption}>Okay</button>
+        <h3 className="modal__title">Selected Option</h3>
+        {props.selectedOption && <p className="modal__body">{props.selectedOption}</p>}
+        <button className="button" onClick={props.handleClearSelectedOption} >Okay</button>
     </Modal>
 );
 
